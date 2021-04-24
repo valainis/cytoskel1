e22923a5a00e3f5d3eee9617a7e023936412ba54
valainis <j.valainis@gmail.com>
2021-04-24 14:50:29 -0700

    use ifdef __name__ == '__main__' for process problem in 3.8

--- test/tgen.py
+++ test/tgen.py
@@ -1,41 +1,42 @@
-import sys
-import time
-import shutil
+if __name__ == '__main__':
+    import sys
+    import time
+    import shutil
 
-import pandas as pd
+    import pandas as pd
 
-#from cytoskel1 import *
-import cytoskel1 as csk1
+    #from cytoskel1 import *
+    import cytoskel1 as csk1
 
-traj_cols = ['CD45', 'CD19', 'IgD', 'CD79b', 'CD20', 'CD34', 'CD179a',
-    'CD72', 'IgM-i', 'Kappa', 'CD10', 'Lambda', 'CD179b', 'CD24', 'CD38',
-    'CD117', 'HLADR', 'IgM-s']
+    traj_cols = ['CD45', 'CD19', 'IgD', 'CD79b', 'CD20', 'CD34', 'CD179a',
+        'CD72', 'IgM-i', 'Kappa', 'CD10', 'Lambda', 'CD179b', 'CD24', 'CD38',
+        'CD117', 'HLADR', 'IgM-s']
 
-avg_cols = ['CD3-1', 'CD3-2', 'CD235-61-66b', 'CD3-3', 'CD45', 'cPARP', 'pPLCg',
-    'CD19', 'CD22', 'pSrc', 'IgD', 'CD79b', 'CD20', 'CD34', 'CD179a', 'pSTAT5',
-    'CD72', 'Ki67', 'IgM-i', 'Kappa', 'CD10', 'Lambda', 'CD179b', 'pAKT', 'CD49d',
-    'CD24', 'CD127', 'RAG1', 'TdT', 'Pax5', 'pSyk', 'pErk12', 'CD38', 'pP38',
-    'CD40', 'CD117', 'pS6', 'CD33-11c-16', 'HLADR', 'IgM-s', 'pCreb']
+    avg_cols = ['CD3-1', 'CD3-2', 'CD235-61-66b', 'CD3-3', 'CD45', 'cPARP', 'pPLCg',
+        'CD19', 'CD22', 'pSrc', 'IgD', 'CD79b', 'CD20', 'CD34', 'CD179a', 'pSTAT5',
+        'CD72', 'Ki67', 'IgM-i', 'Kappa', 'CD10', 'Lambda', 'CD179b', 'pAKT', 'CD49d',
+        'CD24', 'CD127', 'RAG1', 'TdT', 'Pax5', 'pSyk', 'pErk12', 'CD38', 'pP38',
+        'CD40', 'CD117', 'pS6', 'CD33-11c-16', 'HLADR', 'IgM-s', 'pCreb']
 
-df = pd.read_csv("../data/csk8.csv")
+    df = pd.read_csv("../data/csk8.csv")
 
-t0 = time.time()
-csk = csk1.cytoskel("../proj/py_tgen8_1")
-csk.create(df,traj_cols,l1_normalize=True)
+    t0 = time.time()
+    csk = csk1.cytoskel("../proj/py_tgen8_1")
+    csk.create(df,traj_cols,l1_normalize=True)
 
-t1 = time.time()
+    t1 = time.time()
 
-print("setup time",t1 - t0) 
+    print("setup time",t1 - t0) 
 
-csk.do_graphs(n_process=4)
+    csk.do_graphs(n_process=4)
 
-csk.do_branches(-1,4)
+    csk.do_branches(-1,4)
 
-csk.get_average_fix(avg_cols,navg=5,ntree=4)
+    csk.get_average_fix(avg_cols,navg=5,ntree=4)
 
 
-pdir0 = "../proj/py_tgen8"
+    pdir0 = "../proj/py_tgen8"
 
-same,avg_same = csk.check_same(pdir0)
-print(same,avg_same)
-shutil.rmtree(csk.project_dir)
+    same,avg_same = csk.check_same(pdir0)
+    print(same,avg_same)
+    shutil.rmtree(csk.project_dir)
--- test/tlink.py
+++ test/tlink.py
@@ -1,55 +1,56 @@
-import sys
-import time
-import shutil
+if __name__ == '__main__':
+    import sys
+    import time
+    import shutil
 
-import pandas as pd
-import cytoskel1 as csk1
-
-
-traj_cols = ['BRACHYURY', 'HAND1', 'ISL1', 'STAT3_total', 'ZNF207',
-             'PRDM14', 'OCT4', 'TBX6', 'GATA4', 'PBX1', 'cMYC',
-             'NANOG', 'GATA6', 'SOX2', 'LIN28a', 'FOXA2', 'EOMES']
+    import pandas as pd
+    import cytoskel1 as csk1
 
 
+    traj_cols = ['BRACHYURY', 'HAND1', 'ISL1', 'STAT3_total', 'ZNF207',
+                 'PRDM14', 'OCT4', 'TBX6', 'GATA4', 'PBX1', 'cMYC',
+                 'NANOG', 'GATA6', 'SOX2', 'LIN28a', 'FOXA2', 'EOMES']
 
 
-avg_cols = ['barcode1', 'barcode2', 'barcode3', 'barcode4', 'barcode5', 'barcode6',
-    'pHH3_S10', 'A2B5', 'IdU', 'barium', 'cPARP', 'NRF2', 'BRACHYURY', 'HAND1',
-    'ISL1', 'STAT3_total', 'ZNF207', 'CD34', 'PRDM14', 'OCT4', 'TBX6', 'AuroraK_A',
-    'CD326', 'GATA4', 'HIF1a', 'PBX1', 'cMYC', 'Puromycin', 'NANOG', 'GATA6', 'nMYC',
-    'CyclinB1', 'SOX2', 'CD140b', 'GATA3', 'MEIS2', 'LIN28a', 'TWIST1', 'REX1',
-    'CD309_KDR', 'FOXA2', 'H3K27me3', 'JARID2', 'EOMES', 'BRU', 'CD56', 'DNA1', 'DNA2']
 
 
-df = pd.read_csv("../data/tg1.csv")
+    avg_cols = ['barcode1', 'barcode2', 'barcode3', 'barcode4', 'barcode5', 'barcode6',
+        'pHH3_S10', 'A2B5', 'IdU', 'barium', 'cPARP', 'NRF2', 'BRACHYURY', 'HAND1',
+        'ISL1', 'STAT3_total', 'ZNF207', 'CD34', 'PRDM14', 'OCT4', 'TBX6', 'AuroraK_A',
+        'CD326', 'GATA4', 'HIF1a', 'PBX1', 'cMYC', 'Puromycin', 'NANOG', 'GATA6', 'nMYC',
+        'CyclinB1', 'SOX2', 'CD140b', 'GATA3', 'MEIS2', 'LIN28a', 'TWIST1', 'REX1',
+        'CD309_KDR', 'FOXA2', 'H3K27me3', 'JARID2', 'EOMES', 'BRU', 'CD56', 'DNA1', 'DNA2']
 
 
-t0 = time.time()
+    df = pd.read_csv("../data/tg1.csv")
 
 
-csk = csk1.cytoskel("../proj/py_tlink8_1")
-csk.create(df,traj_cols,
-               l1_normalize=True,
-               level_marker='day')
+    t0 = time.time()
 
 
-t1 = time.time()
-
-print("setup time",t1 - t0)
-
-#csk.link("day")
-csk.do_graphs(n_process=4)
+    csk = csk1.cytoskel("../proj/py_tlink8_1")
+    csk.create(df,traj_cols,
+                   l1_normalize=True,
+                   level_marker='day')
 
 
-csk.do_branches(-1,6)
+    t1 = time.time()
 
-csk.get_average_fix(avg_cols,navg=5,ntree=4)
+    print("setup time",t1 - t0)
 
-pdir0 = "../proj/py_tlink8"
+    #csk.link("day")
+    csk.do_graphs(n_process=4)
 
-same,avg_same = csk.check_same(pdir0)
-print(same,avg_same)
-shutil.rmtree(csk.project_dir)
+
+    csk.do_branches(-1,6)
+
+    csk.get_average_fix(avg_cols,navg=5,ntree=4)
+
+    pdir0 = "../proj/py_tlink8"
+
+    same,avg_same = csk.check_same(pdir0)
+    print(same,avg_same)
+    shutil.rmtree(csk.project_dir)
 
 
 
